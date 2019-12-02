5c9c1d3e120440113013a51be458785ebba430e3
Noronha1612 <inc.691@gmail.com>
2019-12-02 00:13:41 -0300

    patch 1.14

--- Estruturas sequenciais/ex012.py
+++
@@ -1,7 +0,0 @@
-from functions.validação import lerFloat
-while True:
-    altura = lerFloat('Digite sua altura em metros: ')
-    if 0.5 <= altura <= 3:
-        break
-    print('Digite uma altura em metros válida.')
-print(f'Seu peso ideal é de {(72.7 * altura) - 58:.1f}Kg')
