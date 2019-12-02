a428f78d44d61d72cf3e86ba9c17d89d7b4fbcdd
Noronha1612 <56375499+Noronha1612@users.noreply.github.com>
2019-12-01 23:04:56 -0300

    Delete ex012.py

--- Estruturas sequenciais/ex012.py
+++
@@ -1,3 +0,0 @@
-from functions.validação import lerOnlyFloat
-altura = lerOnlyFloat('Digite sua altura em métros: ', 'Em metros! ex(1,72)')
-print(f'Seu peso ideal é de {(72.7 * altura) - 58:.1f}Kg')
