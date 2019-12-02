7ebd8cb53828c925a65dd43c6961c65b80e31172
Noronha1612 <inc.691@gmail.com>
2019-12-02 00:07:28 -0300

    patch 1.14

--- Estruturas sequenciais/ex012.py
+++ Estruturas sequenciais/ex012.py
@@ -1,3 +1,7 @@
-from functions.validação import lerOnlyFloat
-altura = lerOnlyFloat('Digite sua altura em métros: ', 'Em metros! ex(1,72)')
+from functions.validação import lerFloat
+while True:
+    altura = lerFloat('Digite sua altura em metros: ')
+    if 0.5 <= altura <= 3:
+        break
+    print('Digite uma altura em metros válida.')
 print(f'Seu peso ideal é de {(72.7 * altura) - 58:.1f}Kg')
