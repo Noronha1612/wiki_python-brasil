2b58112e69a00d9c935c8c9e5821a09a1a3a1aaa
Gabriel Noronha Cavalcante Pessoa <56375499+Noronha1612@users.noreply.github.com>
2020-01-05 00:07:05 -0300

    Revert "patch 3.f"
    This reverts commit 51e8678987c23f6dbffa1759c7b49f229667f190.

--- Estruturas de repetição/ex48.py
+++
@@ -1,6 +0,0 @@
-from functions.validação import lerInt
-
-num = lerInt('Digite um número inteiro positivo: ', pos=True, erro='NÚMERO INTEIRO POSITIVO')
-num = str(num)
-
-print(f'O número digitado invertido é: {num[::-1]}')
