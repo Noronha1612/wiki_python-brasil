56115d20fe9ea6afa85ec8f7fe228a18360531e7
Gabriel Noronha Cavalcante Pessoa <56375499+Noronha1612@users.noreply.github.com>
2020-02-05 22:06:33 -0300

    path 4.11

--- Exercícios Listas/ex11.py
+++ Exercícios Listas/ex11.py
@@ -4,6 +4,7 @@ from functions.visual import tabela
 v1 = []
 v2 = []
 v3 = []
+v4 = []
 
 for x in range(10):
     v1.append(lerFloat(f"Digite o {x + 1}° número do 1° vetor: "))
@@ -13,13 +14,20 @@ print("\n")
 for y in range(10):
     v2.append(lerFloat(f"Digite o {y + 1}° número do 2° vetor: "))
 
+print("\n")
+
+for z in range(10):
+    v3.append(lerFloat(f"Digite o {z + 1}° número do 3° vetor: "))
+
 for i, v in enumerate(v1):
-    v3.append(v)
-    v3.append(v2[i])
+    v4.append(v)
+    v4.append(v2[i])
+    v4.append(v3[i])
 
 tabela({
     "Elementos do 1° vetor": v1,
     "Elementos do 2° vetor": v2,
-    "Elementos do 3° vetor": v3
-}, tam=96)
+    "Elementos do 3° vetor": v3,
+    "Elementos do 4° vetor": v4
+}, tam=154)
 
